105a1ab988de0a9cb7cbd8a05c11936ce21f90b0
gigi <gigi@ua.fm>
2016-02-15 19:14:23 +0200

    Some visual improvments

--- modules/users/views/rbac/_editForm.php
+++ modules/users/views/rbac/_editForm.php
@@ -19,36 +19,40 @@ use yii\helpers\ArrayHelper;
 <?= $form->field($model, 'description')->textarea(['rows' => 3]) ?>
 
 <?php if (!empty($children)) : ?>
-    <h5>Children</h5>
-    <?php //= $form->field($model, 'children')->label($groupLabel)->checkboxList($data) ?>
-    <?php foreach($children as $groupLabel => $data) : ?>
-        <div class="form-group field-roles-children <?=$model->hasErrors('children') ? 'has-error' : ''?>">
-            <label class="control-label col-sm-2" for="roles-children"><?=$groupLabel?></label>
+    <fieldset>
+        <legend>Children</legend>
+        <?php //= $form->field($model, 'children')->label($groupLabel)->checkboxList($data) ?>
+        <?php foreach($children as $groupLabel => $data) : ?>
+            <div class="form-group field-roles-children <?=$model->hasErrors('children') ? 'has-error' : ''?>">
+                <label class="control-label col-sm-2" for="roles-children"><?=$groupLabel?></label>
                 <div class="col-sm-5">
                     <div id="roles-children">
-                    <?=Html::checkboxList(\common\components\Helper::getBaseClassName($model) . '[children]', $model->children, $data, [
-                        'encode' => false,
-                        'item' => function ($index, $label, $name, $checked, $value) {
-                            $checkbox = Html::checkbox($name, $checked, [
-                                'value' => $value,
-                                'label' => $label
-                            ]);
-                            return Html::tag('div', $checkbox, ['class' => 'checkbox']);
-                        }
-                    ]) ?>
+                        <?=Html::checkboxList(\common\components\Helper::getBaseClassName($model) . '[children]', $model->children, $data, [
+                            'encode' => false,
+                            'item' => function ($index, $label, $name, $checked, $value) {
+                                $checkbox = Html::checkbox($name, $checked, [
+                                    'value' => $value,
+                                    'label' => $label
+                                ]);
+                                return Html::tag('div', $checkbox, ['class' => 'checkbox']);
+                            }
+                        ]) ?>
+                    </div>
                 </div>
             </div>
+        <?php endforeach ?>
+        <div class="col-sm-offset-2 col-sm-5 has-error">
+            <div class="help-block">
+                <?php foreach($model->getErrors('children') as $error) : ?>
+                    <?=$error?>
+                <?php endforeach; ?>
+            </div>
         </div>
-    <?php endforeach ?>
-    <div class="col-sm-offset-2 col-sm-5 has-error">
-        <div class="help-block">
-            <?php foreach($model->getErrors('children') as $error) : ?>
-                <?=$error?>
-            <?php endforeach; ?>
-        </div>
-    </div>
-<?php endif; ?>
-<div class="form-group">
+        <?php endif; ?>
+
+    </fieldset>
+
+    <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
